9bb4c4074f8b0c647c4cee90b71d319912367fa5
Christopher Hart <christopherjhart95@gmail.com>
2026-03-13 14:03:37 +0000

    refactor: generalize IOS-XE dir parser registration

--- src/muninn/parsers/iosxe/dir.py
+++ src/muninn/parsers/iosxe/dir.py
@@ -1,4 +1,4 @@
-"""Parser for 'dir' and 'dir crashinfo:' commands on IOS-XE."""
+"""Parser for 'dir' commands on IOS-XE."""
 
 import re
 from typing import NotRequired, TypedDict
@@ -55,9 +55,9 @@ def _build_file_entry(match: re.Match[str]) -> FileEntry:
 
 
 @register(OS.CISCO_IOSXE, "dir")
-@register(OS.CISCO_IOSXE, "dir crashinfo:")
+@register(OS.CISCO_IOSXE, r"dir (?P<filesystem>\S+)")
 class DirParser(BaseParser[DirResult]):
-    """Parser for 'dir' command.
+    """Parser for 'dir' command output.
 
     Example output:
         Directory of bootflash:/
